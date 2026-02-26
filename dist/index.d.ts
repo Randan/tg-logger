5694df7e4bcc5877304ad2c7dd6be2c3688ee270
Andrew Randan <andrewrandan@gmail.com>
2026-02-26 15:30:41 +0200

    Add .gitignore, remove dist and node_modules from tracking

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { LoggerModule } from './logger/logger.module';
-export { LoggerService } from './logger/logger.service';
-export { NotifyAdminModule } from './notify-admin/notify-admin.module';
-export { NotifyAdminService } from './notify-admin/notify-admin.service';
--- dist/logger/logger.service.d.ts
+++
@@ -1,9 +0,0 @@
-import type { ConfigService } from '@nestjs/config';
-export declare class LoggerService {
-    private readonly config;
-    private readonly isDev;
-    constructor(config: ConfigService);
-    log(...args: unknown[]): void;
-    error(...args: unknown[]): void;
-    warn(...args: unknown[]): void;
-}
